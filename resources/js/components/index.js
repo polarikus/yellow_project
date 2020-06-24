516dce0f50823b8c660d8b8da59c5b3129096b29
Polarikus <igoran9607@gmail.com>
2020-06-24 20:32:08 +0300

    Start project

---
+++ resources/js/components/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Dutylist extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">Привет желтая команда!</div>
+
+                            <div className="card-body">Я компонент React! =)</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+if (document.getElementById('react')) {
+    ReactDOM.render(<Dutylist />, document.getElementById('react'));
+}
