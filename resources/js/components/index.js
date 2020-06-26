d447bb5f03829f2ed15bd434139e17e4e66a2eed
Polarikus <igoran9607@gmail.com>
2020-06-27 00:26:00 +0300

    Создана модель User Написаны 2 метода api для получения пользователей

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -8,7 +8,7 @@ export default class Dutylist extends Component {
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card">
-                            <div className="card-header">Привет желтая команда!</div>
+                            <div className="card-header">Привет еще раз</div>
 
                             <div className="card-body">Я компонент React! =)</div>
                         </div>
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+//Получить всех пользователей
+
+Route::get('/user', 'User\UserController@getUsers')->name('GetUsers');
+
+//Получить одного пользователя по id
+
+Route::get('/user/{id}', 'User\UserController@getUserById')->name('GetUserById');
