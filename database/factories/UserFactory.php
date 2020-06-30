6f85b524a31068e65c252a71bd70f8692424e228
Polarikus <igoran9607@gmail.com>
2020-06-30 19:48:32 +0300

    Фабрики

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,9 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
+        'login' => $faker->userName,
         'name' => $faker->name,
+        'phone' => $faker->numberBetween(0,799999999),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
