e787fef380d918c0c5fffd6b166a3a9642d50ad3
Игорь Данилов <59930161+polarikus@users.noreply.github.com>
2020-06-30 20:25:21 +0300

    Update TaskFactory.php

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -4,12 +4,12 @@
 
 use App\Models\Task;
 use App\Models\Team;
-
+use App\Models\TaskType;
+use App\Models\Status;
 use Faker\Generator as Faker;
 
 $factory->define(Task::class, function (Faker $faker) {
     return [
-
         'team_id' => factory(Team::class),
         'task_type_id' => rand(1,2),
         'name' => $faker->jobTitle,
